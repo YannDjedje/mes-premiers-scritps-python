41710550c4ca1d650299cd47fb09df1e1a3608bb
Yann Djedje <yanndjedje14@gmail.com>
2026-09-16 09:09:46 +0000

    premiercode.py

---
+++ Premiercode.py
@@ -0,0 +1,7 @@
+print("Bonjout tout le monde")
+print("l'envers du decor")
+print("je suis une autre phrase")
+# je suis un commentaire d'une seul ligne
+""" je suis un commentaire  a plusieur ligne.
+et me voila sur une autre ligne""" 
+print("je suis MR \n le protocole")
